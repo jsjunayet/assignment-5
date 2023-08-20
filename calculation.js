e9644166df4b36a731ab0ae46e6889a0350ea18e
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2023-08-20 19:38:39 +0600

    first commit

---
+++ calculation.js
@@ -0,0 +1,103 @@
+document.getElementById('div1').addEventListener('click',function(){
+    const main = document.getElementById('main')
+    const cardTitle1 = document.getElementById('card-1-title')
+    const cardText1 = cardTitle1.innerText;
+    const li = document.createElement('li')
+    li.innerText = cardText1;
+    li.classList.add('class1')
+    main.appendChild(li);
+    const taka1 = document.getElementById('taka-1');
+    taka1Sting = parseFloat(taka1.innerText);
+    const previousTaka = document.getElementById('total-price')
+    const previousTakaSring = parseFloat(previousTaka.innerText);
+    const totaltaka = previousTakaSring + taka1Sting;
+    const pricetaka = previousTaka.innerText = totaltaka.toFixed(2)
+    previousTaka.classList.add('class2')
+    
+    
+})
+document.getElementById('div2').addEventListener('click',function(){
+    const main = document.getElementById('main')
+    const cardTitle1 = document.getElementById('card-2-title')
+    const cardText1 = cardTitle1.innerText;
+    const li = document.createElement('li')
+    li.innerText = cardText1;
+    li.classList.add('class1')
+    main.appendChild(li);
+    const taka1 = document.getElementById('taka-2');
+    taka1Sting = parseFloat(taka1.innerText);
+    const previousTaka = document.getElementById('total-price')
+    const previousTakaSring = parseFloat(previousTaka.innerText);
+    const totaltaka = previousTakaSring + taka1Sting;
+    const pricetaka = previousTaka.innerText = totaltaka.toFixed(2)
+    previousTaka.classList.add('class2')
+    
+})
+document.getElementById('div3').addEventListener('click',function(){
+    const main = document.getElementById('main')
+    const cardTitle1 = document.getElementById('card-3-title')
+    const cardText1 = cardTitle1.innerText;
+    const li = document.createElement('li')
+    li.innerText = cardText1;
+    li.classList.add('class1')
+    main.appendChild(li);
+    const taka1 = document.getElementById('taka-3');
+    taka1Sting = parseFloat(taka1.innerText);
+    const previousTaka = document.getElementById('total-price')
+    const previousTakaSring = parseFloat(previousTaka.innerText);
+    const totaltaka = previousTakaSring + taka1Sting;
+    const pricetaka = previousTaka.innerText = totaltaka.toFixed(2)
+    previousTaka.classList.add('class2')
+    
+})
+document.getElementById('div4').addEventListener('click',function(){
+    const main = document.getElementById('main')
+    const cardTitle1 = document.getElementById('card-4-title')
+    const cardText1 = cardTitle1.innerText;
+    const li = document.createElement('li')
+    li.innerText = cardText1;
+    li.classList.add('class1')
+    main.appendChild(li);
+    const taka1 = document.getElementById('taka-4');
+    taka1Sting = parseFloat(taka1.innerText);
+    const previousTaka = document.getElementById('total-price')
+    const previousTakaSring = parseFloat(previousTaka.innerText);
+    const totaltaka = previousTakaSring + taka1Sting;
+    const pricetaka = previousTaka.innerText = totaltaka.toFixed(2)
+    previousTaka.classList.add('class2')
+    
+})
+document.getElementById('div5').addEventListener('click',function(){
+    const main = document.getElementById('main')
+    const cardTitle1 = document.getElementById('card-5-title')
+    const cardText1 = cardTitle1.innerText;
+    const li = document.createElement('li')
+    li.innerText = cardText1;
+    li.classList.add('class1')
+    main.appendChild(li);
+    const taka1 = document.getElementById('taka-5');
+    taka1Sting = parseFloat(taka1.innerText);
+    const previousTaka = document.getElementById('total-price')
+    const previousTakaSring = parseFloat(previousTaka.innerText);
+    const totaltaka = previousTakaSring + taka1Sting;
+    const pricetaka = previousTaka.innerText = totaltaka.toFixed(2)
+    previousTaka.classList.add('class2')
+    
+})
+document.getElementById('div6').addEventListener('click',function(){
+    const main = document.getElementById('main')
+    const cardTitle1 = document.getElementById('card-6-title')
+    const cardText1 = cardTitle1.innerText;
+    const li = document.createElement('li')
+    li.innerText = cardText1;
+    li.classList.add('class1')
+    main.appendChild(li);
+    const taka1 = document.getElementById('taka-6');
+    taka1Sting = parseFloat(taka1.innerText);
+    const previousTaka = document.getElementById('total-price')
+    const previousTakaSring = parseFloat(previousTaka.innerText);
+    const totaltaka = previousTakaSring + taka1Sting;
+    const pricetaka = previousTaka.innerText = totaltaka.toFixed(2)
+    previousTaka.classList.add('class2')
+    
+})
